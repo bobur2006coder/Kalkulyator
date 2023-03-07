bb1f0d97a0fa46ddd55dd642a87d1d544a5a8408
bobur2006coder <>
2023-03-07 11:52:26 +0500

    yuksaldi

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ var num1 = +prompt("Birinchi soni kiriting")
 var numpr =prompt("Amalni kiriting")
 var num2 = +prompt("Ikkinchi soni kiriting")
 if(numpr=="+"){
-    console.log(num1+num2);
-}else if(numpr=="-"){
-console.log(num1-num2);
-}else if(numpr=="*"){
-    console.log(num1*num2);
-    }else if(numpr=="/"){
-        console.log(num1 / num2);
+    alert("Misolning javobi :"  +(num1+num2));
+}else if("Misolning javobi :"  +(numpr=="-")){
+alert("Misolning javobi :"  +(num1-num2));
+}else if("Misolning javobi :"  +(numpr=="*")){
+    alert("Misolning javobi :"  +(num1*num2));
+    }else if("Misolning javobi :"  +(numpr=="/")){
+        alert("Misolning javobi :"  +(num1 / num2));
         }else{
-            console.log("siz amalni yoki sonni kiritmadingiz");
+            alert("Siz amalni yoki sonni kiritmadingiz");
         }
